8db4514625d5aa7aa8a52e839329c4a1a2772c00
Shivesh Dixit <dixitshivesh55@gmail.com>
2023-04-30 00:06:52 +0530

    passing props into card component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
+import athlete from "./images/athlete.png"
 function App() {
   return (
     <div >
   
             <Navbar /> 
             <Hero /> 
+            <Card 
+                img={athlete}
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import React from "react"
+import athlete from "../images/athlete.png"
+import star from "../images/star.png"
+/*
+Challenge: Build the Card component
+For now, hard-code in the data (like 
+the rating, title, price, etc.)
+
+Notes:
+- Only render 1 instance (I already did this for you)
+- The star icon and photo (katie-zaferes.png) are in the images 
+  folder for your use
+- Make sure to include:
+    - image
+    - star icon (star.png), rating, and review count
+    - title
+    - cost/person
+- The main purpose of this challenge is to show you where our limitations
+  currently are, so don't worry about the fact that you're hard-coding all
+  this data into the component.
+*/
+
+export default function Card(props) {
+  console.log(props);
+    return (
+        <div className="card">
+        <img src={props.img} className="card--image" />
+        <div className="card--stats">
+            <img src={star}  className="card--star" />
+            <span className="gray" >{props.rating}</span>
+            <span className="gray">(props.reviewCount) • </span>
+            <span>{props.country}</span>
+        </div>
+         <p>{props.title}</p>
+            <p><span className="bold">From ${props.price} / person  </span></p>
+
+    </div>
+    )
+}
